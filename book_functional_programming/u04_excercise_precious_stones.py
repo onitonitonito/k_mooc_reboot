609046581c7f2e060c5baa7dbfca4eb83cee77c0
O-nitix3 on Earth <nitt0x0@gmail.com>
2018-01-14 21:13:13 +0900

    9.6-add udemy lecture exercise u03,u04,u05

--- book_functional_programming/u04_excercise_precious_stones.py
+++ book_functional_programming/u04_excercise_precious_stones.py
@@ -7,36 +7,33 @@ If there are more than 5 precious stones, delete the first stone and store
 the new one.
 """
 class PreciousStone(object):
+    # this is class attribute, (not self)
     number_of_precious_stones = 0
     precious_stone_collection = []
 
     def __init__(self, name):
         self.name = name
-        # Increment the number of preciousStones
         PreciousStone.number_of_precious_stones += 1
 
-        # Append the precious stone to the list if total number of stones are less than 5
         if PreciousStone.number_of_precious_stones <= 5:
             PreciousStone.precious_stone_collection.append(self)
         else:
-            # If more than 5 stones are present, delete the first one and store the new one
             del PreciousStone.precious_stone_collection[0]
             PreciousStone.precious_stone_collection.append(self)
 
     @staticmethod
     def display_precious_stones():
-        for preciousStone in PreciousStone.precious_stone_collection:
-            print(preciousStone.name, end = ' ')
+        for precious_stone in PreciousStone.precious_stone_collection:
+            print(precious_stone.name, end = ', ')
         print()
 
-preciousStoneOne  = PreciousStone("Ruby")
-preciousStoneTwo  = PreciousStone("Emerald")
-preciousStoneThree  = PreciousStone("Sapphire")
-preciousStoneFour  = PreciousStone("Diamond")
-preciousStoneFive  = PreciousStone("Amber")
+precious_stone_1  = PreciousStone("Ruby")
+precious_stone_2  = PreciousStone("Emerald")
+precious_stone_3  = PreciousStone("Sapphire")
+precious_stone_4  = PreciousStone("Diamond")
+precious_stone_5  = PreciousStone("Amber")
 
-preciousStoneFive.display_precious_stones()
+precious_stone_5.display_precious_stones()
 
-# Print all the stones after deleting the first stone
-preciousStoneSix = PreciousStone("Onyx")
-preciousStoneSix.display_precious_stones()
+precious_stone_6 = PreciousStone("Onyx")
+precious_stone_6.display_precious_stones()
