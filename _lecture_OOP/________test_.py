e90f9ebcb4709719ad09e6d293ddf3b67b9ce229
Where is the Onito Sandiago <nitt0x0@gmail.com>
2018-04-14 10:33:19 +0900

    12.3-add oop_lecture, _name_space.py

--- _lecture_OOP/________test_.py
+++ _lecture_OOP/________test_.py
@@ -9,6 +9,47 @@ def price():
     return locals()
 
 for key, value in price().items():
-    print("%s = %s"%(key,value))
+    print("%-10s %s"%(key,value))
 
 price = property(**price())
+
+class One(object):
+    def __init__(self, x=10, y=20):
+        self.first_var = x
+        self.second_var = y
+
+    def add(self):
+        return self.first_var + self.second_var
+
+    def multi(self):
+        return self.first_var * self.second_var
+
+    @staticmethod
+    def show_operands(obj):
+        print("%s + %s = %s"% (obj.first_var,obj.second_var,obj.add()))
+        print("%s x %s = %s"% (obj.first_var,obj.second_var,obj.multi()))
+
+    def local(self):
+        return locals()
+
+t = One()
+# t.show_operands(t)
+
+a = One(1024, 64)
+# a.show_operands(a)
+
+
+for key, value in One.__dict__.items():
+    print("%-17s %s"%(key, value))
+print()
+
+# ----------------------------------------------------------------
+for key, value in globals().items():
+    print("%-17s %s"%(key, value))
+print()
+
+for key, value in locals().items():
+    print("%-17s %s"%(key, value))
+print()
+
+print("Globas = locals? :", globals() == locals())         # True
