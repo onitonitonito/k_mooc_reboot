5301929e4ad67dfdf066130dc1712f6b0d169f4c
Onito3 on The Earth! <nitt0x0@gmail.com>
2018-09-17 12:33:12 +0900

    13.1-excel odd even making

--- module_openpyxl/excel_write.py
+++ module_openpyxl/excel_write.py
@@ -1,33 +1,36 @@
+"""
+* 엑셀 설치없이 엑셀시트 만들기
+* 임의의 시트를 활성화해서 내용을 기록하고 새로운 액셀화일을 만듬
+"""
+# script run 에서는 실행이 안됨.
 
-import os
 import datetime
-from openpyxl import Workbook
+import openpyxl as opx
 
-# WORKING DIR. SET
-DIRS = os.path.dirname(__file__).partition("k_mooc_reboot")
-ROOT = DIRS[0] + DIRS[1] + '\\'
+wb = opx.Workbook()
 
-wb = Workbook()
-
+# sheet = wb["Sheet1"]      # or
 # grab the active worksheet
-ws = wb.active
+sheet = wb.active
 
 # Data can be assigned directly to cells
-ws['A1'] = 'Python OpenPyXL module TEST'
-
 # Python types will automatically be converted
-ws['B1'] = datetime.datetime.now()
+sheet['A1'] = 'Python OpenPyXL module test'
+sheet['F1'] = datetime.datetime.now()
 
 # Rows can also be appended
-ws.append([1, 2, 3, 4, 5])
-ws.append(['Hello!', 'This','is the','OpenPyXL','World!'])
+sheet.append([1, 2, 3, 4, 5])
+sheet.append(['Hello!', 'This', 'is the', 'OpenPyXL', 'World!'])
 
+command_list = sheet['A1'].__dir__()
+for content in command_list:
+    if content[:1] is "_":
+        pass
+    else:
+        print(content)
 
-for content in ws['A1'].__dir__():
-    print(content)
-
-print(ws['A1'].value)       # Python OpenPyXL module TEST
-print(ws['B1'].value)       # datetime.now()
+print(sheet['A1'].value)       # Python OpenPyXL module TEST
+print(sheet['F1'].value)       # datetime.now()
 
 # Save the file
-wb.save(ROOT + "_static/_sample.xlsx")
+wb.save("./_static/_sample.xlsx")
---
+++ module_openpyxl/odd_even_write.py
@@ -0,0 +1,53 @@
+"""
+* 엑셀작업 줄이기 - 엑셀없이 코드로 수정/정리/추출/쓰기
+* OpenPyXL = xlsx 지원, 읽기/쓰기
+* XlsxWriter (화일생성, 서버사이드) = xlsx 지원
+* Xlrd, Xlwt, xlwings, pywin32 = xls 지원 (Win, Mac, Linux)
+* xlwings (W,M), pywin32 (W) - 설치된 엑셀필요
+"""
+# script run 에서는 실행이 안됨.
+
+import pandas as pd
+import openpyxl as opx
+
+TARGET_XLS = "./_static/_total_example.xls"
+OUTPUT_01 = "./_static/_u_even.xls"
+OUTPUT_02 = "./_static/_u_odd.xls"
+TARGET_XLSX = "./_static/_new_example.xlsx"
+
+WB = opx.load_workbook(TARGET_XLSX)
+SHEET = WB["Sheet1"]
+# sheet = wb.get_sheet_by_name("Sheet1")        # deprecated
+
+
+def excel_inout(target, out1, out2):
+    """
+    * DOCS::pandas.read_excel = https://goo.gl/Dy1A5z
+    """
+    df = pd.read_excel(target)
+    df[0::2].to_excel(out1)
+    df[1::2].to_excel(out2)
+
+
+def show_cell(sheet):
+    """
+    * print(sheet['A1'].value, sheet['B1'].value)
+    """
+    for i in range(1, 30):
+        print(sheet['A' + str(i)].value, " = ", sheet['B' + str(i)].value)
+
+    print()
+    print("[1,1] =", sheet.cell(row=1, column=1).value)
+    print("[1,2] =", sheet.cell(row=1, column=2).value)
+
+
+def show_overview(sheet):
+    """전체 입력란을 오버뷰"""
+    for row in sheet.rows:
+        print([col.value for col in row])
+
+
+if __name__ == '__main__':
+    excel_inout(TARGET_XLS, OUTPUT_01, OUTPUT_02)
+    # show_cell(SHEET)
+    # show_overview(SHEET)
