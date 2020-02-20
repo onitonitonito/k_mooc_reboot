1fa63d2d22ad24a22f5aafad2558ea37a8a426c2
Kay Suparx <nitt0x0@gmail.com>
2020-02-20 18:22:27 +0900

    24.1-PyQt5 Ex5.12 QTabWidget Advanced

---
+++ module_PyQt/tech_lectures/ex5/ex512_QTabWidget.py
@@ -0,0 +1,60 @@
+"""
+# Ex 5.12_QTabWidget.py
+http://codetorial.net/pyqt5/widget/qtabwidget.html
+"""
+# ...
+#  - QLineEdit
+#  - QLineEdit (Advanced)
+#  - QProgressBar
+#  - QSlider & QDial
+#  - QSplitter
+#  - QGroupBox
+#  - QTabWidget *
+#  - QTabWidget (Advanced)
+#  - QPixmap
+# ...
+
+print(__doc__)
+
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+                        QApplication,
+                        QWidget,QTabWidget,
+                        QVBoxLayout,
+                    )
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Ex5.11_QGroupBox'
+        self.posXY = (500, 50)
+        self.windowSize = (500, 400)
+        self.initUI()
+
+    def initUI(self):
+        tab1 = QWidget()
+        tab2 = QWidget()
+
+        tabs = QTabWidget()
+        tabs.addTab(tab1, 'Tab1')
+        tabs.addTab(tab2, 'Tab2')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(tabs)
+
+        self.setLayout(vbox)
+
+        self.show_basic()
+
+
+    def show_basic(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(*self.posXY, *self.windowSize)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyApp()
+    sys.exit(app.exec_())
---
+++ module_PyQt/tech_lectures/ex5/ex513_QTabWidget_advenced.py
@@ -0,0 +1,139 @@
+"""
+# Ex 5.13_QTabWidget_advenced.py
+http://codetorial.net/pyqt5/widget/qtabwidget_advanced.html
+"""
+# ...
+#  - QLineEdit
+#  - QLineEdit (Advanced)
+#  - QProgressBar
+#  - QSlider & QDial
+#  - QSplitter
+#  - QGroupBox
+#  - QTabWidget
+#  - QTabWidget (Advanced) *
+#  - QPixmap
+# ...
+
+print(__doc__)
+
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+
+class MyApp(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Ex5.13_QTabWidget_advenced'
+        self.posXY = (500, 50)
+        self.windowSize = (350, 300)
+        self.initUI()
+
+    def initUI(self):
+        tabs = QTabWidget()
+        tabs.addTab(FirstTab(), 'First')
+        tabs.addTab(SecondTab(), 'Second')
+        tabs.addTab(ThirdTab(), 'Third')
+
+        buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttonbox.accepted.connect(self.accept)
+        buttonbox.rejected.connect(self.reject)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(tabs)
+        vbox.addWidget(buttonbox)
+
+        self.setLayout(vbox)
+
+        self.show_basic()
+
+    def show_basic(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(*self.posXY, *self.windowSize)
+        self.show()
+
+class FirstTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        name = QLabel('Name:')
+        nameedit = QLineEdit()
+        age = QLabel('Age:')
+        ageedit = QLineEdit()
+        nation = QLabel('Nation:')
+        nationedit = QLineEdit()
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(name)
+        vbox.addWidget(nameedit)
+        vbox.addWidget(age)
+        vbox.addWidget(ageedit)
+        vbox.addWidget(nation)
+        vbox.addWidget(nationedit)
+        vbox.addStretch()
+
+        self.setLayout(vbox)
+
+class SecondTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        lan_group = QGroupBox('Select Your Language')
+        combo = QComboBox()
+        list = ['Korean', 'English', 'Chinese']
+        combo.addItems(list)
+
+        vbox1 = QVBoxLayout()
+        vbox1.addWidget(combo)
+        lan_group.setLayout(vbox1)
+
+        learn_group = QGroupBox('Select What You Want To Learn')
+        korean = QCheckBox('Korean')
+        english = QCheckBox('English')
+        chinese = QCheckBox('Chinese')
+
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(korean)
+        vbox2.addWidget(english)
+        vbox2.addWidget(chinese)
+        learn_group.setLayout(vbox2)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lan_group)
+        vbox.addWidget(learn_group)
+        self.setLayout(vbox)
+
+class ThirdTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        lbl = QLabel('Terms and Conditions')
+        text_browser = QTextBrowser()
+        text_browser.setText('This is the terms and conditions')
+        checkbox = QCheckBox('Check the terms and conditions.')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl)
+        vbox.addWidget(text_browser)
+        vbox.addWidget(checkbox)
+
+        self.setLayout(vbox)
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyApp()
+    sys.exit(app.exec_())
