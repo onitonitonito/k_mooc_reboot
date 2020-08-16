659bbab595fbd8224045bf149f6c793d39fe9b8f
Kay Suparx <nitt0x0@gmail.com>
2020-08-16 17:56:44 +0900

    33.8-modif _path

--- openCV_TAcademy/lecture_01/simple_show.py
+++ openCV_TAcademy/lecture_01/simple_show.py
@@ -1,21 +1,37 @@
 """
-# image read
+# IMAGE READ/SHOW : NO USE OF MATPLOTLIB
+# BE CAREFUL OF RGB <-> BGR
 """
-
-import cv2
-import matplotlib.pyplot as plt
-
-from _path import get_cut_dir
+# src(RGB) -> cv2(BGR) <-> plt(RGB)
 
 print(__doc__)
 
+import cv2
+import matplotlib.pyplot as plt
+from _path import (DIR_SRC, get_cut_dir, stop_if_none)
 
-dir_home = get_cut_dir('openCV_TAcademy')
-img = cv2.imread(dir_home + '/src/dir_home + cat.bmp')
+# VARIOUS IMAGE READUNG : cv2 <-> plt
+src = cv2.imread(DIR_SRC + 'cat.bmp', cv2.IMREAD_UNCHANGED)
+src = stop_if_none(src, message='Image loading failed!')
+
+src2 = plt.imread(DIR_SRC + 'cat.bmp', format='RGB')
+src2 = stop_if_none(src2, message='Image loading failed!')
+
+srcRGB = cv2.cvtColor(src2, cv2.COLOR_BGR2RGB)
+srcRGB = stop_if_none(srcRGB, message='Image loading failed!')
+
 
 # img type = ndarray
-print(type(img))
+[print(f"TYPE = {type(obj)}") for obj in [src, src2, srcRGB]]
 
-# check image on window
-plt.imshow(img)
+# cv2 imshow()
+cv2.imshow('src', src)
+cv2.imshow('src2', src2)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# matplotlib = check image on window
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,7), )
+ax.imshow(src)
 plt.show()
